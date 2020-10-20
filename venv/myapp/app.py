00f662ce2cbfd2d2696b04842b8604ad3adc0aef
Guhan Ganesan <phpguhan@gmail.com>
2020-10-20 21:52:55 +0530

    Template inheritance

--- venv/myapp/app.py
+++ venv/myapp/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
+List = ["Raja","Anbu","Ganesan","Guhan"]
+
 @app.route('/test')
 def test():
     return "Hi Raja"
@@ -8,8 +10,12 @@ def test():
 @app.route('/')
 def index():
     name = "Guhan Ganesan"
-    return render_template('index.html', user=name)
- 
+    return render_template('index.html',user=name)
+
+@app.route('/concepts')
+def concept():
+    name = "Guhan Ganesan"
+    return render_template('concepts.html', user=name, len=len(List), mylist=List)
 	
 if __name__=="__main__":
   app.run(debug = True)
