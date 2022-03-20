12ca5b56eb5b58d7267072548ec5b1e91b0f4ca7
guhanganesan <raghavaguhan@gmail.com>
2022-03-20 22:28:01 +0530

    insert query for postgresql

--- venv/myapp/app.py
+++ venv/myapp/app.py
@@ -47,29 +47,59 @@ def get_db_connection():
 @app.route('/get_employee')
 @cross_origin()
 def get_db():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM employee;')
-    emp = cur.fetchall()
-    cur.close()
-    conn.close()
-    print(emp)
-    #return render_template('index.html', employee=emp)
-    emp_data = []
-  
-    for emp in emp:
-            print(emp[0])
-            print(emp[1])
-            print(emp[2])
-            print(emp[3])
-            print(emp[4])
-            temp = {"id":emp[0],"name":emp[1], "age":emp[2], "address":emp[3], "salary":emp[4]}
-            emp_data.append(temp)
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM employee;')
+        emp = cur.fetchall()
+        cur.close()
+        conn.close()
+        print(emp)
+        #return render_template('index.html', employee=emp)
+        emp_data = []
+    
+        for emp in emp:
+                temp = {"id":emp[0],"name":emp[1], "age":emp[2], "address":emp[3], "salary":emp[4]}
+                emp_data.append(temp)
 
-    return jsonify({
-        "status":"success",
-        "data": emp_data
-    })
+        return jsonify({
+            "status":"success",
+            "data": emp_data
+        })
+
+    except (Exception, psycopg2.Error) as error:
+        return jsonify({
+            "status":"error",
+            "data": "Error while fetching employee details"
+        })
+        
+@app.route('/publish_employee', methods=['POST'])
+@cross_origin()
+def publish_employee_details():
+    try:
+        #print("====================")
+        body = request.get_json()
+        #print(body)
+        conn = get_db_connection()
+        cur = conn.cursor()
+        emp_data = list(body.get("employee_data"))
+        for items in emp_data:
+            #print(items.get('name'))
+            postgres_insert_query = """ INSERT INTO  publish_employee(id, name, age, address, salary) VALUES (%s,%s,%s,%s,%s)"""
+            record_to_insert = (int(items.get('id')),items.get('name'), int(items.get('age')), items.get('address'), int(items.get('salary')))
+            cur.execute(postgres_insert_query, record_to_insert)
+            conn.commit()
+        if conn:
+            return jsonify({
+                "status":"success",
+                "msg": "successfully published employee details"
+            })
+
+    except (Exception, psycopg2.Error) as error:
+        return jsonify({
+            "status":"error",
+            "data": "Error while submitting data. please check connection"
+        })
         
         
 
